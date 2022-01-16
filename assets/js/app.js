0424ff0e5535a2da037de6a52b499a34915ab4cf
Alex Paredes <alexparedesr95@gmail.com>
2022-01-15 22:42:52 -0600

    FEAT: A brief check to how to declare and see variables.

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+let a = 10,
+    b = 20,
+    c = 'Hola ',
+    d = 'Hulk',
+    x = a+b;
+
+const saludo = c+d;
+
+console.log({a});
+console.log({b});
+console.log({c});
+console.table({a, b, c, d, x});
+
+c = 'Hola de nuevo.';
