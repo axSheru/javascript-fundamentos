fc1500d2af3db3bb5116723eb4b12bb3f3b2ebdf
Alex Paredes <alexparedesr95@gmail.com>
2022-01-16 22:51:22 -0600

    FEAT: Added examples of resrved words.

---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,15 @@
+// Nombres no válidos.
+
+let const = 0;
+let await = 0;
+let for = ';';
+let while = 0;
+let class = 0;
+
+
+// Nombres válidos.
+
+let _hola = 0;
+let algo$ = 0;
+let ho_88_54 = 0;
+let estaEsUnaVariable = '0';
