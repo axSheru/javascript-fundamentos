08057fbba8e3c2c6573b217e4b5bc8c439127470
Alex Paredes <alexparedesr95@gmail.com>
2022-01-17 22:27:27 -0600

    FEAT: Added basic examples of array creation and array access.

---
+++ assets/js/arreglos.js
@@ -0,0 +1,28 @@
+// Se crea un arreglo de distintas formas.
+/* const arr = new Array(10);
+const arr = [];
+console.log(arr) */
+
+let videoJuegos = [ 'Mario 3', 'Megaman', 'Chrono Trigger' ];
+console.log({ videoJuegos })
+
+console.log( videoJuegos[0] )
+
+let arregloCosas = [
+    true,
+    123,
+    'Alex',
+    3.5 + 7,
+    function(){},
+    ()=>{},
+    { a: 1 },
+    ['X', 'Megaman', 'Zero', 'Dr. Light', [
+        'Dr. Willy',
+        'Woodman',
+    ],],
+];
+
+console.log({ arregloCosas })
+console.log( arregloCosas[2] )
+console.log( arregloCosas[7][3] )
+console.log( arregloCosas[7][4][1] )
