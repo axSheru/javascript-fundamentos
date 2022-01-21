ed5d48eedb336880efb57bd08e0ac5ec05a2116e
Alex Paredes <alexparedesr95@gmail.com>
2022-01-20 20:42:21 -0600

    FEAT: Added examples of conditionals optimized with objects and arrays.

--- assets/js/if-else.js
+++ assets/js/if-else.js
@@ -24,3 +24,28 @@ if ( dia === 0 ) {
 } else {
     console.log('Hoy no es domingo, lunes o martes.');
 }
+
+const diasSemanaArr = [
+    'Domingo',
+    'Lunes',
+    'Martes',
+    'Miércoles',
+    'Jueves',
+    'Viernes',
+    'Sábado'
+];
+
+const diasSemanaObj = {
+    0: 'Domingo',
+    1: 'Lunes',
+    2: 'Martes',
+    3: 'Miércoles',
+    4: 'Jueves',
+    5: 'Viernes',
+    6: 'Sábado',
+};
+
+let diaSemana = 3;
+
+console.log( diasSemanaArr[ diaSemana ] ||  'Día no válido.');
+console.log( diasSemanaObj[ diaSemana ] ||  'Día no válido.');
