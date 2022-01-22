1059eb1406b17dda7b9788b44e2e42c3046e3a1a
Alex Paredes <alexparedesr95@gmail.com>
2022-01-21 21:55:14 -0600

    FEAT: Added examples with the switch statement.

---
+++ assets/js/switch.js
@@ -0,0 +1,18 @@
+const dia = 2;
+
+switch (dia) {// Esto compara con ===
+    case 0:
+        console.log( 'Domingo' );
+        break;// Se utiliza para salir de la estructura de control.
+    case 1:
+        console.log( 'Lunes' );
+        break;
+    case 2:
+        console.log( 'Martes' );
+        break;
+    default:
+        console.log( 'No es domingo, lunes o martes' );
+        break;
+}
+
+
