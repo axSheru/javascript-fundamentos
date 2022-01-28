3f75be7c6ee864b634b3bfb9c9a6c8618814e9a4
Alex Paredes <alexparedesr95@gmail.com>
2022-01-27 20:15:18 -0600

    FEAT: Added class functions examples.

--- assets/js/classes/class.js
+++ assets/js/classes/class.js
@@ -10,11 +10,23 @@ class Persona {
         this.sable = sable;
     }
 
+    quienSoy() {
+        console.log(`Soy ${ this.nombre } y mi afiliación es ${ this.afiliacion }`);
+    }
+
+    miFrase() {
+        this.quienSoy();
+        console.log(`El sable de ${ this.nombre } es de tono ${ this.sable }`);
+    }
+
 }
 
 const mace = new Persona( 'Mace Windu', 'Jedi', 'Morado' );
 const sidious = new Persona( 'Darth Sidious', 'Sith', 'Rojo' );
 console.log(mace);
 console.log(sidious);
+
+mace.miFrase();
+sidious.miFrase();
 
 
